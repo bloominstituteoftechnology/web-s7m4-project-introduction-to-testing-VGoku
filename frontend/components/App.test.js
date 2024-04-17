27fb8722b207050b0e2a19f2a9a286c1e456ca15
David S. <neojohn2312@gmail.com>
2024-04-17 17:21:41 -0500

    FirstPush

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import App from './App'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
+import txt from '../i18n/index.json';
 
 describe('Module 4 Project Tests', () => {
   describe('English Language', () => {
@@ -11,34 +12,83 @@ describe('Module 4 Project Tests', () => {
       One test is done for you as an example.
     */
     test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
-      render(<App lang="en" />)
-      expect(screen.getByText("Create an Account")).toBeVisible()
-    })
-  })
+      render(<App lang="en" />);
+      expect(screen.getByText(txt.en.TEXT_HEADING_CREATE_ACCOUNT)).toBeVisible();
+    });
+
+    // Testing other text elements
+    // test(`${key} is visible`, () => {
+    //   render(<App />)
+    //   expect(screen.getByText(val)).toBeVisible()
+    // })
+
+    // LABEL_USERNAME
+    test(`LABEL_USERNAME is visible`, () => {
+      render(<App lang="en" />);
+      expect(screen.getByLabelText(txt.en.LABEL_USERNAME)).toBeVisible();
+    });
+
+    // PLACEHOLDER_USERNAME
+    test(`PLACEHOLDER_USERNAME is visible`, () => {
+      render(<App lang="en" />);
+      expect(screen.getByPlaceholderText(txt.en.PLACEHOLDER_USERNAME)).toBeVisible();
+    });
+  });
+
   describe('Spanish Language', () => {
     /*
       👉 TASK 3
 
       This is done after making the UI multilingual.
     */
-  })
+    test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByText(txt.esp.TEXT_HEADING_CREATE_ACCOUNT)).toBeVisible();
+    });
+
+    // LABEL_USERNAME
+    test(`LABEL_USERNAME is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByLabelText(txt.esp.LABEL_USERNAME)).toBeVisible();
+    });
+
+    // PLACEHOLDER_USERNAME
+    test(`PLACEHOLDER_USERNAME is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByPlaceholderText(txt.esp.PLACEHOLDER_USERNAME)).toBeVisible();
+    });
+  });
+
   describe('getEntriesByKeyPrefix', () => {
     test('can extract the correct data', () => {
-    /*
-      👉 TASK 4 part 2
+      const obj = {
+        abc_1: "data_abc_1",
+        abc_2: "data_abc_2",
+        xyz_1: "data_xyz_1",
+        abc_3: "data_abc_3",
+      };
 
-      Implement the function `getEntriesByKeyPrefix` below
-      and then come back here and write a few tests
-      to ensure it works as expected.
+      const expectedABC =  [
+        ["abc_1", "data_abc_1"],
+        ["abc_2", "data_abc_2"],
+        ["abc_3", "data_abc_3"],
+      ];
 
-      Although it should be noted that commonly,
-      the tests are written _before_ implementing
-      the function being tested.
-    */
-    })
-  })
-})
+      const expectedXYZ = [
+        ["xyz_1", "data_xyz_1"],
+      ];
+
+      const expectedEmpty = [];
+
+      expect(getEntriesByKeyPrefix(obj, "abc")).toEqual(expectedABC);
+      expect(getEntriesByKeyPrefix(obj, "xyz")).toEqual(expectedXYZ);
+      expect(getEntriesByKeyPrefix(obj, "foo")).toEqual(expectedEmpty);
+    });
+  });
+});
+
 function getEntriesByKeyPrefix(obj, keyPrefix) {
+  return Object.entries(obj).filter(([key]) => key.startsWith(keyPrefix + "_"));
   /*
     👉 TASK 4 part 1
 
